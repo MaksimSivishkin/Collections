8320f2275e099d60a74e0d4a0d8d4c1679e42d04
Sivishkin Maksim <MSivishkin127@gmail.com>
2025-03-31 17:08:34 +0300

    Коллекции - Задание 2

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,24 +1,38 @@
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 
 public class Start {
-    public static void main(String[] args) {
-        List<Integer>integerList = new ArrayList<>();
-        integerList.add(1);
-        integerList.add(2);
-        integerList.add(3);
-        integerList.add(4);
-        integerList.add(5);
-        integerList.add(6);
-
-
-        for(int i=0; i<integerList.size(); i++){
-            if(i%2==0){
-                Collections.swap(integerList, i, i+1);
+    public static void bubbleSort(ArrayList<Integer> intList) {
+        for (int i = 0; i < intList.size() - 1; i++) {
+            for (int j = 0; j < intList.size() - i - 1; j++) {
+                if (intList.get(j) > intList.get(j + 1)) {
+                    int temp = intList.get(j);
+                    intList.set(j, intList.get(j + 1));
+                    intList.set(j + 1, temp);
+                }
             }
         }
-        System.out.println(integerList);
+    }
+
+    public static void main(String[] args) {
+        int[] intArray = {11,1,45,6,3,67,2,6};
+
+        boolean isSorted = false;
+        int buf;
+        while (!isSorted){
+            isSorted=true;
+            for (int i = 0; i < intArray.length - 1; i++){
+                if (intArray[i]>intArray[i+1]) {
+                    isSorted=false;
+
+                    buf=intArray[i];
+                    intArray[i]=intArray[i+1];
+                    intArray[i+1]=buf;
+                }
+            }
+        }
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+
+        }
     }
 }
